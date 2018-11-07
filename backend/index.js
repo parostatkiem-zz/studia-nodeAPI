a429195008f3c05d55784aea46e169d624cec5ca
Sudczak, Jan <jan.sudczak@sap.com>
2018-11-07 16:22:56 +0100

    Remove obsolete steps

--- backend/index.js
+++ backend/index.js
@@ -33,43 +33,6 @@ app.get('/api/cars', (req, res) => {
   });
 });
 
-//add new car
-app.post('/api/car', (req, res) => {
-  const { brand, model } = req.body;
-  if (!brand || !model) {
-    res.status(402).send({
-      success: 'false',
-      message: 'not all params provided',
-    });
-    return;
-  }
-  const last_id = Math.max.apply(Math, cars_array.map(car => car.id));
-  cars_array = [...cars_array, { id: last_id + 1, brand, model }];
-  res.status(200).send({
-    success: 'true',
-    message: 'car added',
-  });
-});
-
-// delete single car
-app.delete('/api/car/:id', (req, res) => {
-  const id = req.params.id;
-  const cars_new = cars_array.filter(car => car.id != id);
-
-  if (cars_new.length === cars_array.length) {
-    res.status(409).send({
-      success: 'false',
-      message: 'Could not remove car with given id',
-    });
-    return;
-  }
-  cars_array = [...cars_new];
-  res.status(200).send({
-    success: 'true',
-    message: 'car removed',
-  });
-});
-
 const PORT = 5000;
 
 app.listen(PORT, () => {
